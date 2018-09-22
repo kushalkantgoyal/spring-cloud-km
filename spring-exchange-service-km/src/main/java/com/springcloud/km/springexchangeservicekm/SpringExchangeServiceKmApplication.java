9d85dbe94eb94fd1f37f905f3a036e0c129182a6
Kushal Kant Goyal <kushalkantgoyal@gmail.com>
2018-09-22 13:50:57 +0530

    Currency exchange service created

---
+++ spring-exchange-service-km/src/main/java/com/springcloud/km/springexchangeservicekm/SpringExchangeServiceKmApplication.java
@@ -0,0 +1,12 @@
+package com.springcloud.km.springexchangeservicekm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExchangeServiceKmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringExchangeServiceKmApplication.class, args);
+	}
+}
